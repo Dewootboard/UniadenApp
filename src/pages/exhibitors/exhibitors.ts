19e6229a1ccd0d344c8c2775c50783c6de41f884
Dewootboard <jockehemma@hotmail.com>
2017-11-28 18:11:52 +0100

    All pages are now in unique folders

---
+++ src/pages/exhibitors/exhibitors.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-about',
+  templateUrl: 'exhibitors.html',
+})
+
+export class Exhibitors {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
---
+++ src/pages/masskarta/masskarta.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  templateUrl: 'masskarta.html'
+})
+export class MassKarta {
+  constructor(public navCtrl: NavController) {}
+}
