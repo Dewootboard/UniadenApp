5de13ea514bf631f111ec52f9bfe3690380d1835
Dewootboard <jockehemma@hotmail.com>
2017-11-28 14:32:34 +0100

    Navigeringsimplementation, inte klar. Länk till hemsidan klar

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -5,10 +5,24 @@ import { NavController } from 'ionic-angular';
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController) {}
 
+  goToOtherPage() {
+    //push another page onto the history stack
+    //causing the nav controller to animate the new page in
+    this.navCtrl.push(OtherPage);
   }
 
+}
+
+@Component({
+  templateUrl: `Masskarta.html`
+})
+class OtherPage {
+  constructor(){
+    
+  }
 }
